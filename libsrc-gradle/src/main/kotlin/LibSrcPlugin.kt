ff79810c3ce0ad04c6f6b1b173dc0a959d36dee4
Eugene Petrenko <me@jonnyzzz.com>
2023-12-26 16:16:23 +0100

    add extension to the plugin

--- libsrc-gradle/src/main/kotlin/LibSrcPlugin.kt
+++ libsrc-gradle/src/main/kotlin/LibSrcPlugin.kt
@@ -2,11 +2,18 @@ package com.jonnyzzz.libsrc
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.provider.Provider
 
 @Suppress("unused")
 class LibSrcPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         println("Hello from libsrc plugin")
+
+        @Suppress("SpellCheckingInspection")
+        val extension = project.extensions.create("libsrc", LibSrcExt::class.java)
     }
+}
+
+open class LibSrcExt {
 }
 
