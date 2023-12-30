1a8ace11cf59fa2d3075512684c8abb4ab406f44
Eugene Petrenko <me@jonnyzzz.com>
2023-12-30 22:32:46 +0100

    playground-2 problem example (task is not called)

--- demo/playground-2/build.gradle.kts
+++ demo/playground-2/build.gradle.kts
@@ -1,3 +1,16 @@
 plugins {
     `java-library`
 }
+
+val buildMe by tasks.creating {
+    outputs.upToDateWhen { false }
+    doLast {
+        println("This task is called on import")
+    }
+}
+
+dependencies {
+    implementation(fileTree(projectDir) {
+        builtBy(buildMe)
+    })
+}
